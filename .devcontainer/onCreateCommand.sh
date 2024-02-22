1abab30c546973e69d85a5d6e9a6b5a17442490a
sycod <julmat2@gmail.com>
2024-02-22 12:56:42 +0000

    devcontainer update

--- .devcontainer/onCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-# .devcontainer/onCreateCommand.sh
-# apply once at the beginning of VM creation
-
-# update apt
-sudo apt update && sudo apt -y upgrade
-
-# create Python virtual environment
-python -m venv ~/.env
--- .devcontainer/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-# .devcontainer/postCreateCommand.sh
-# apply once at the end of VM creation
-
-# launch Python virtual environment on startup
-echo "source ~/.env/bin/activate" >> ~/.bashrc
-
-# alias
-echo "alias ll='ls -alF'" >> ~/.bashrc
