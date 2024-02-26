6eb3c0a2b1a23062563bf3ef66327ec9dd857f08
sycod <julmat2@gmail.com>
2024-02-26 11:56:10 +0100

    test mlflow.autolog()

--- mlflow_folder/my_mlf_script.py
+++ mlflow_folder/my_mlf_script.py
@@ -2,16 +2,32 @@
 
 import mlflow
 
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+
+
 def main():
-    client = mlflow.MlflowClient()
+    mlflow.autolog()
 
-    mlflow.log_param("threshold", 4)
-    mlflow.log_param("verbosity", "INFO")
+    db = load_diabetes()
+    X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
 
-    mlflow.log_metric("ma_metrique", 666)
-    mlflow.log_metric("TimeToCompute", 1)
+    rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
+    # MLflow triggers logging automatically upon model fitting
+    rf.fit(X_train, y_train)
 
-    mlflow.log_artifact("my_artifact.csv")
+
+
+    # client = mlflow.MlflowClient()
+
+    # mlflow.log_param("threshold", 4)
+    # mlflow.log_param("verbosity", "INFO")
+
+    # mlflow.log_metric("ma_metrique", 666)
+    # mlflow.log_metric("TimeToCompute", 1)
+
+    # mlflow.log_artifact("my_artifact.csv")
 
 
 if __name__ == '__main__':
