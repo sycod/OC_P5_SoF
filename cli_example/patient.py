360593f5db9309811ab73c96c02b58ab311718cf
sycod <julmat2@gmail.com>
2024-03-04 15:11:21 +0000

    Fix print statements and update date calculation

--- cli_example/patient.py
+++ cli_example/patient.py
@@ -1,3 +1,4 @@
+#/usr/bin/env python
 """A click test module."""
 
 import click
@@ -9,13 +10,15 @@ import click
 def pid(patient_id, verbose):
     """Displays information from a patient ID."""
     message = f"Patient ID {patient_id}, verbose {'on' if verbose else 'off'}"
+    print(message)
     return message
 
 
 def another_function():
     """Another function's help, to test API."""
-
-    return "Another function's print."
+    message = "Another function's print."
+    print(message)
+    return message
 
 
 if __name__ == "__main__":
