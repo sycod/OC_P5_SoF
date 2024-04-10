eab80d7a6661753d24b4882f53c38769d5c2125e
sycod <julmat2@gmail.com>
2024-04-10 17:16:33 +0000

    unsupervised models in progress

---
+++ src/models.py
@@ -0,0 +1,24 @@
+"""Functions used by models"""
+
+import numpy as np
+
+
+def results_from_vec_matrix(vectorizer, X, n_max) -> dict:
+    """Predict a maximum of n_max results from a vectorizer and a transformed sparse matrix X."""
+    d = X.data
+    i = X.indices
+
+    # get highest weights
+    weights = np.sort(d)[::-1][: min(len(i), n_max)].tolist()
+
+    # get corresponding indices
+    pred_indices = [i[np.argsort(-d)][: min(len(i), n_max)]]
+
+    # get corresponding words
+    preds = [vectorizer.get_feature_names_out()[x] for x in pred_indices[0]]
+
+    return {"tokens": preds, "weights": weights}
+
+
+if __name__ == "__main__":
+    print(f"\n👉 results_from_vec_matrix(values) -> str\n{results_from_vec_matrix.__doc__}")
