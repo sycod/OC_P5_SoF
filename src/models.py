8ef19ebab1d4dbd507f868699ce6f775e0d6125d
sycod <julmat2@gmail.com>
2024-04-16 18:13:03 +0000

    scoring ok

--- src/models.py
+++ src/models.py
@@ -1,10 +1,23 @@
 """Functions used by models"""
 
+import time
 import numpy as np
+from Levenshtein import ratio
+from sklearn.manifold import TSNE
 
 
-def results_from_vec_matrix(vectorizer, X, n_max) -> dict:
-    """Predict a maximum of n_max results from a vectorizer and a transformed sparse matrix X."""
+def score_terms(pred_words, target_words, cutoff=0.7) -> float:
+    """Return a score of terms similarity between 2 lists of strings"""
+    score = 0
+    for p_w in pred_words:
+        score += max(ratio(t, p_w, score_cutoff=cutoff) for t in target_words)
+    score = np.round(score / len(pred_words), 3)
+    
+    return score
+    
+
+def results_from_vec_matrix(words_list, X, n_max=5) -> dict:
+    """Predict a maximum of n_max results from a vectorized transformed sparse matrix X."""
     d = X.data
     i = X.indices
 
@@ -15,9 +28,39 @@ def results_from_vec_matrix(vectorizer, X, n_max) -> dict:
     pred_indices = i[np.argsort(-d)][: min(len(i), n_max)]
 
     # get corresponding words
-    preds = [vectorizer.get_feature_names_out()[x] for x in pred_indices]
+    preds = [words_list[x] for x in pred_indices]
 
     return list(zip(preds, weights, pred_indices))
+
+
+def get_5_tags_from_matrix(words_list, X, n_max=5) -> list:
+    """Predict a maximum of n_max words from a vectorized transformed sparse matrix X."""
+    # get corresponding indices
+    pred_indices = X.indices[np.argsort(-X.data)][: min(len(X.indices), n_max)]
+    # get corresponding words
+    preds = [words_list[x] for x in pred_indices]
+
+    return preds
+
+
+def score_reduce(words_list, X, y, n_groups=5) -> tuple:
+    """Returns model results from words list, features and targets"""
+    start_time = time.time()
+
+    # get scores from results
+    X_results = [get_5_tags_from_matrix(words_list, xi) for xi in X]
+    scores = [score_terms(p_w, y[i].split(" ")) for i, p_w in enumerate(X_results)]
+    model_score = np.round(np.mean(scores), 3)
+
+    # reduce dimensions
+    tsne = TSNE(n_components=2, perplexity=50, n_iter=2000, init='random', learning_rate=200, random_state=42)
+    X_tsne = tsne.fit_transform(X)
+
+    duration = np.round(time.time() - start_time,0)
+
+    print("Score: ", model_score, "- Duration: ", duration)
+    
+    return model_score, X_results, scores, X_tsne
 
 
 def get_lda_topics(model, feature_names, n_top_words) -> list:
