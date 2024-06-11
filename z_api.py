5882fbcaec9d516d6ff3adafb040d7bd9b23ed28
Julien LEMAIRE <julien.lemaire@capgemini.com>
2024-06-11 18:48:34 +0200

    api ok

--- z_api.py
+++ z_api.py
@@ -4,15 +4,15 @@ import os
 import dill as pickle
 import logging
 import streamlit as st
+from streamlit_tags import st_tags
+from streamlit_tags import st_tags_sidebar
+import re
+import emoji
 
 # ML
+import numpy as np
 from gensim.models import Word2Vec
 import nltk
-
-# home made functions
-from src.scrap_and_clean import preprocess_doc
-from src.models import w2v_vect_data
-from src.models import lr_predict_tags
 
 
 # CONFIG
@@ -78,11 +78,14 @@ if "classifier" not in st.session_state:
         logging.warning(f"⚠️ No classifier found ⚠️")
 # placeholders (if not in session state)
 if "title_input" not in st.session_state:
-    st.session_state.title_input = TITLE_PLACEHOLDER
+    st.session_state.title_input = ""
 if "body_input" not in st.session_state:
-    st.session_state.body_input = BODY_PLACEHOLDER
+    st.session_state.body_input = ""
 if "predicted_tags" not in st.session_state:
     st.session_state.predicted_tags = TAGS_PLACEHOLDER
+if "message" not in st.session_state:
+    st.session_state.message = None
+
 
 # update session state on inputs
 def update_title():
@@ -91,76 +94,180 @@ def update_body():
     st.session_state.body_input = st.session_state.body
 
 
-
-
-
-# def check_doc(input_doc) -> :
-# 🚧 MAKE FUNCTION TO CHECK INPUT FIRST (data must be at least 2 words long, not punctuation:
-#     # check
-#     else:
-#         check = True
-#     return check
+def check_length(input_doc, length=5) -> bool:
+    """Check input length"""
+    length_ok = True if len(input_doc.split(" ")) >= length else False
     
+    return length_ok
 
 
-# def preprocess_doc(document, keep_set, exclude_set) -> str:
-#     🚧 packages used -> re, nltk
-#     🚧 regrouper fonctions en une seule
-#     🚧 include keep_set and exclude_set in function
-#     doc_clean = clean_string(document)
-#     doc_tokens = tokenize_str(doc_clean, keep_set, exclude_set)
-#     doc_lemmed = lemmatize_tokens(doc_tokens, keep_set, exclude_set)
-#     doc_tk_clean = clean_tokens(doc_lemmed, keep_set, exclude_set)
-#     doc_preprocessed = " ".join(doc_tk_clean)
+def preprocess_doc(document, keep_set, exclude_set) -> str:
+    """Preprocess document for a complete cleaning"""
+    # CLEAN DOC
+    # remove code tags
+    document = re.sub(r"<code>.*?<\/code>", "", document, flags=re.S)
+    # remove img tags
+    document = re.sub(r"<img.*?>", "", document)
+    # remove all html tags
+    document = re.sub(r"<.*?>", "", document)
+    # remove emojis
+    document = emoji.replace_emoji(document, replace=" ")
+    # remove newlines
+    document = re.sub(r"\n", " ", document)
+    # lowercase
+    document = document.lower()
+    # remove suspension points
+    document = re.sub(r"\.\.\.", " ", document)
+    # remove digits only tokens
+    document = re.sub(r"\b(?<![0-9-])(\d+)(?![0-9-])\b", " ", document)
+    # remove multiple spaces
+    document = re.sub(r" +", " ", document)
 
-#     return doc_preprocessed
 
-
-def predict_tags(input_doc) -> str:
-    """🚧 """
-    input_clean = preprocess_doc(
-        input_doc,
-        st.session_state.keep_set,
-        st.session_state.exclude_set
+    # TOKENIZE DOC
+    # tokenize except excluded
+    tokens = nltk.word_tokenize(document)
+    # remove hashes from watch list
+    i_offset = 0
+    for i, t in enumerate(tokens):
+        i -= i_offset
+        if t == "#" and i > 0:
+            left = tokens[: i - 1]
+            joined = [tokens[i - 1] + t]
+            right = tokens[i + 1 :]
+            if joined[0] in keep_set:
+                tokens = left + joined + right
+                i_offset += 1
+    # remove (< 3)-letter words apart from those appearing in keep_set
+    tokens_rm_inf3 = [t for t in tokens if len(t) > 2 or t in keep_set]
+    # remove tokens containing absolutely no letter
+    tokens_rm_no_letter = list(
+        filter(lambda s: any([c.isalpha() for c in s]), tokens_rm_inf3)
     )
+    # remove remaining excluded words
+    tokens_cleaned = [t for t in tokens_rm_no_letter if t not in exclude_set]
 
-    # 🚧 supprimer les outputs : les supprimer des fonctions
-    logging.info(f"User input: {input_doc}")
-    logging.info(f"\nClean input: {input_clean}")
 
+    # LEMMATIZE TOKENS
+    kilmister = nltk.wordnet.WordNetLemmatizer()
+    lem_tok_list = []
+
+    for token in tokens_cleaned:
+        if token in keep_set:
+            lem_tok_list.append(token)
+        else:
+            lem_tok = kilmister.lemmatize(token)
+            if lem_tok not in exclude_set:
+                lem_tok_list.append(lem_tok)
+
+
+    # CLEAN TOKENS
+    # clean " ' " in front of certain words
+    clean_apo = []
+    clean_apo += [t[1:] if t[0] == "'" else t for t in lem_tok_list]
+    # clean " - " in front of certain words
+    clean_dash = []
+    clean_dash += [t[1:] if t[0] == "-" else t for t in clean_apo]
+    # remove (< 3)-letter words apart from those belonging to keep_set
+    tokens_rm_inf3 = [t for t in clean_dash if len(t) > 2 or t in keep_set]
+    # remove remaining excluded words
+    tokens_cleaned = [t for t in tokens_rm_inf3 if t not in exclude_set]
+
+    doc_preprocessed = " ".join(tokens_cleaned)
+
+    return doc_preprocessed
+
+
+def w2v_vect_data(model, matrix) -> np.array:
+    """From a Word2Vec vectorizer, return a vectorized matrix"""
+    # loop over rows in tokenized X_train
+    doc_vectors = []
+    for tokens in matrix:
+        # loop over tokens in each row
+        doc_vec = []
+        for token in tokens:
+            if token in model.wv:
+                doc_vec.append(model.wv[token])
+        # mean it
+        doc_vectors.append(np.mean(doc_vec, axis=0))
+    # get X_train matrix
+    vector_matrix = np.array(doc_vectors)
+
+    return vector_matrix
+
+
+def lr_predict_tags(model, X, n_tags=5) -> list:
+    """Use logistic regression probabilities to get at least n predicted tags"""
+    ppbs = model.predict_proba(X)
+    classes = model.classes_
+    pred_tags = []
+
+    for i, x in enumerate(X):
+        # create list of tags from n first classes
+        pred_list = (
+            (" ")
+            .join([classes[c] for c in ppbs[i].argsort()[: -n_tags - 1 : -1]])
+            .split(" ")
+        )
+        # keep only 5 first tags
+        pred = set()
+        j = 0
+        while len(pred) < 5:
+            pred.add(pred_list[j])
+            j += 1
+        # add tags to predictions list
+        pred_tags.append((" ").join(pred))
+
+    return pred_tags
+
+
+def predict_tags(input_clean) -> str:
+    """Predict tags from  an input preprocessed data"""
     X_vect = w2v_vect_data(st.session_state.vectorizer, [input_clean.split(" ")])
+    lr_preds = lr_predict_tags(st.session_state.classifier, X_vect)
+    predictions = str.join("  ", lr_preds)
+
+    # log infos
     logging.info(f"X shape: {X_vect.shape}")
     logging.info(f"Vectors:\n{X_vect[0]}")
-
-    lr_preds = lr_predict_tags(st.session_state.classifier, X_vect)
-    predictions = str.join(" ", lr_preds)
     logging.info(f"Predictions: {predictions}")
 
     return predictions
 
 
 def click_button():
-    """🚧 """
+    """Actions to perform when button clicked"""
     user_input = st.session_state.title_input + "\n" + st.session_state.body_input
 
-    # check user input
-    # checked_doc = check_doc(input_doc)
-    # if checked_doc == True:
-    #     logging.info("✅ user input checked")
-    #     pass
-    # else:
-    #     logging.warning(f"⚠️ error {checked_doc}")
-    #     return checked_doc
-    # predict tags
-    st.session_state.predicted_tags = predict_tags(user_input)
+    # check user input length
+    if not check_length(user_input):
+        logging.warning(f"⚠️ Input length is too short")
+        st.session_state.predicted_tags = None
+        st.session_state.message = "⚠️ Input length is too short"
+    else:
+        # preprocess input
+        input_clean = preprocess_doc(
+            user_input,
+            st.session_state.keep_set,
+            st.session_state.exclude_set
+        )
 
+        # check preprocessed input length before predict
+        if not check_length(input_clean):
+            logging.warning(f"⚠️ Length is too short after preprocessing: check input")
+            st.session_state.predicted_tags = None
+            st.session_state.message = "⚠️ Length is too short after preprocessing: check input"
+        else:
+            # predict tags
+            predicted_tags = predict_tags(user_input)
+            st.session_state.predicted_tags = predicted_tags
 
+        # log infos
+        logging.info(f"User input: {user_input}")
+        logging.info(f"\nClean input: {input_clean}")
+        logging.info(f"\nPredicted tags: {st.session_state.predicted_tags}")
 
-
-
-
-
-
+    return st.session_state.predicted_tags
 
 
 # GUI
@@ -177,8 +284,16 @@ st.text_input("Title", placeholder=TITLE_PLACEHOLDER, key="title", on_change=upd
 st.text_area("Body", placeholder=BODY_PLACEHOLDER, height=160, key="body", on_change=update_body)
 
 # predictions
-st.button('⬇️ Predict tags ⬇️', type='primary', use_container_width=True, on_click=click_button)
-st.write("### :blue[{}]".format(st.session_state.predicted_tags))
+st.button('⬇️  Predict tags  ⬇️', type='primary', use_container_width=True, on_click=click_button)
 
-st.write("🚧ℹ️ TIP: It includes a note about valid characters")
-# preciser "too many frequent words" ou "balises HTML supprimées" ou "modèle entraîné sur de l'anglais"...
+if st.session_state.predicted_tags is not None:
+    st.write("#### :blue[{}]".format(st.session_state.predicted_tags))    
+else:
+    st.write("#### :red[{}]".format(st.session_state.message))
+
+st.divider()
+
+st.write("## ℹ️ TIPS")
+st.markdown("""- preprocessing discards many **frequent and usual words** plus **HTML tags** and **code snippets** from user sentences and may result to a too small final input.  
+    An error message can thus be displayed.""")
+st.write("- Also note that the **model is trained for english language** input and may result in weird predictions in other cases.")
